f9f03ba7fe8acfc04d867d5e0c270143c91146b0
saiakhil <saiakhil90@gmail.com>
2021-05-19 12:56:38 +0530

    Update route.tf

--- modules/api-gateway-route/route.tf
+++ modules/api-gateway-route/route.tf
@@ -2,7 +2,7 @@
 resource "aws_apigatewayv2_route" "api_gw_route" {
   count     = length(var.api_gw_route_keys)
   api_id    = var.api_gateway_id
-  route_key = element(var.api_gw_route_keys,count.index)
+  route_key = var.api_gw_route_keys[count.index]
   target    = "integrations/${var.integration_id}"
 }
 
